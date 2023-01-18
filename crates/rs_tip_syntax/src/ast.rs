b9af9b3f6cdd82725b0cd8a917e2c4e9021214be
manapointer <manapointer@gmail.com>
2023-01-18 00:54:12 -0800

    finish ast

--- crates/rs_tip_syntax/src/ast.rs
+++ crates/rs_tip_syntax/src/ast.rs
@@ -7,10 +7,28 @@ pub(crate) struct Spanned<T> {
 pub(crate) type AstInt = Spanned<i32>;
 pub(crate) type AstString = Spanned<String>;
 pub(crate) type AstExpr = Spanned<Expr>;
+pub(crate) type AstField = Spanned<Field>;
 pub(crate) type AstStmt = Spanned<Stmt>;
+pub(crate) type AstFunction = Spanned<Function>;
+pub(crate) type AstProgram = Spanned<Program>;
+
+pub(crate) struct Program {
+    functions: Vec<AstFunction>,
+}
+
+pub(crate) struct Function {
+    name: AstString,
+    parameters: Vec<AstString>,
+    variables: Vec<AstString>,
+    statements: Vec<AstStmt>,
+    return_: AstExpr,
+}
 
 pub(crate) enum Stmt {
-    Assign(AstString, AstExpr),
+    IdentifierAssign(AstString, AstExpr),
+    PointerAssign(AstExpr, AstExpr),
+    FieldAssign(AstString, AstString, AstExpr),
+    DereferenceFieldAssign(AstExpr, AstString, AstExpr),
     Output(AstExpr),
     If(AstExpr, Box<AstStmt>, Option<Box<AstStmt>>),
     While(AstExpr, Box<AstStmt>),
@@ -19,11 +37,28 @@ pub(crate) enum Stmt {
 pub(crate) enum Expr {
     Int(AstInt),
     Identifier(AstString),
+    Unary(UnOp, Box<AstExpr>),
     Binary(Box<AstExpr>, BinOp, Box<AstExpr>),
     Input,
+    Call(Box<AstExpr>, Vec<AstExpr>),
+    Alloc(Box<AstExpr>),
+    Pointer(AstString),
+    Dereference(Box<AstExpr>),
+    Null,
+    Record(Vec<AstField>),
+    Field(Box<AstExpr>, AstString),
 }
 
-pub enum BinOp {
+pub(crate) struct Field {
+    name: AstString,
+    value: Box<AstExpr>,
+}
+
+pub(crate) enum UnOp {
+    Negative,
+}
+
+pub(crate) enum BinOp {
     Add,
     Subtract,
     Multiply,
